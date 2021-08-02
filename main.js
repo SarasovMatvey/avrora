fe6dbb425aeabf2a95e1901b4cf52d20897aa565
sarasov matvey <sarasovmatvej@gmail.com>
2021-08-02 21:31:58 +0600

    added base functions of rendering

--- main.js
+++ main.js
@@ -1,31 +1,86 @@
-const WIDTH = 2;
-const HEIGHT = 2;
+const WIDTH = 900;
+const HEIGHT = 'auto';
 const SPACE_BORDER_WIDTH = 2;
-const SPACE_BORDER_WIDTH = 2;
-const SPACE_BUSY = '#bb2124';
-const SPACE_AVAILABLE = '#22bb33';
+const SPACE_BORDER_BUSY = '#bb2124';
+const SPACE_BORDER_AVAILABLE = '#22bb33';
+const SPACE_FILL_BUSY = 'rgba(187, 33, 36, 0.5)';
+const SPACE_FILL_AVAILABLE = 'rgba(34, 187, 51, 0.5)';
+const ACTIVE_FLOOR_INDEX = 0;
 
 class FloorsScheme {
-  
   constructor(wrapper, data) {
-    this.wrapper = wrapper;
-    this.ctx = canvas.getContext('2d');
+    this.wrapper = $(wrapper);
     this.data = data;
   }
-  
+
   initialize() {
-    this.canvas = $('<canvas></canvas>')
-    $(this.wrapper).append(this.canvas);
-    this.ctx = $('#scheme')[0].getContext('2d');
-    this._drawRect(10, 10, 200, 200, this.SPACE_BUSY);
+    this.canvas = $('<canvas></canvas>');
+    this.wrapper.append(this.canvas[0]);
+
+    this.wrapper.css('position', 'relative');
+    this.wrapper.css('width', WIDTH);
+    this.wrapper.css('height', HEIGHT);
+    this.canvas.css('position', 'relative');
+    this.canvas.css('zIndex', '1');
+    console.log(this.wrapper.width());
+    this.canvas.css('width', this.wrapper.width());
+    this.canvas.css('height', this.wrapper.height());
+
+    this.ctx = this.canvas[0].getContext('2d');
+
+    for (let i = 0; i < this.data.length; i++) {
+      const floorImage = this.data[i].floorImage;
+      this.wrapper.append(
+        `<img class="floor-image" src="${floorImage}" data-index="${i}" hidden/>`
+      );
+    }
+
+    this.changeActiveFloor(ACTIVE_FLOOR_INDEX);
   }
 
-  _drawRect(x, y, width, height, color) {
+  changeActiveFloor(floorIndex) {
+    const floorImage = this.wrapper.find(
+      `.floor-image[data-index="${floorIndex}"]`
+    );
+    this.wrapper.find('.floor-image').attr('hidden', true);
+    floorImage.attr('hidden', false);
+
+    this._showFloorSpaces(floorIndex);
+  }
+
+  _addSpace(coords, type) {
     this.ctx.beginPath();
-    this.ctx.lineWidth = this.SPACE_BORDER_WIDTH;
-    this.ctx.strokeStyle = color;
-    this.ctx.rect(x, y, width, height);
+
+    this.ctx.setLineDash([5, 5]);
+    this.ctx.lineWidth = SPACE_BORDER_WIDTH;
+    switch (type) {
+      case 'busy':
+        this.ctx.strokeStyle = SPACE_BORDER_BUSY;
+        this.ctx.fillStyle = SPACE_FILL_BUSY;
+        break;
+      case 'available':
+        this.ctx.strokeStyle = SPACE_BORDER_AVAILABLE;
+        this.ctx.fillStyle = SPACE_FILL_AVAILABLE;
+        break;
+      default:
+        this.ctx.strokeStyle = SPACE_BORDER_BUSY;
+        this.ctx.fillStyle = SPACE_FILL_BUSY;
+        break;
+    }
+
+    for (const [x, y] of coords) {
+      this.ctx.lineTo(x, y);
+    }
     this.ctx.stroke();
-    this.ctx.closePath();
+
+    this.ctx.fill();
+  }
+
+  _showFloorSpaces(floorIndex) {
+    this.ctx.clearRect(0, 0, this.canvas[0].width, this.canvas[0].height);
+
+    for (const spaceInfo of this.data[floorIndex].spacesInfo) {
+      this._addSpace(spaceInfo.spaceCoords, spaceInfo.spaceType);
+    }
   }
 }
